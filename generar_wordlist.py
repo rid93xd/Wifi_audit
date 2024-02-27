55aea1ff83c5478fbb090852f19d4f86d912a7c4
rid93xd <118381242+rid93xd@users.noreply.github.com>
2024-02-27 20:15:20 -0300

    generar_wordlist.py

--- generar_wordlist.py
+++ generar_wordlist.py
@@ -1,28 +1,21 @@
-# Lista para almacenar palabras con 8 o más caracteres
-palabras_largas = []
-
 # Solicita al usuario la dirección del archivo
 archivo = input("Por favor, introduce la ruta del archivo que contiene la lista de palabras: ")
 
 try:
-    # Abre el archivo en modo lectura, especificando 'latin-1' como el encoding
-    with open(archivo, 'r', encoding='latin-1') as f:
-        # Lee cada línea del archivo
-        for linea in f:
-            # Elimina los espacios en blanco al principio y al final de la línea
-            palabra = linea.strip()
-            # Verifica si la palabra tiene 8 o más caracteres y la agrega a la lista palabras_largas
-            if len(palabra) >= 8:
-                palabras_largas.append(palabra)
-
-    # Define la ruta del archivo de salida
-    archivo_salida = "palabras_largas.txt"
     
-    # Abre el archivo de salida en modo escritura
-    with open(archivo_salida, 'w') as f:
-        # Escribe cada palabra en una línea del archivo de salida
-        for palabra in palabras_largas:
-            f.write(palabra + '\n')
+    archivo_salida = "palabras_largas.txt"
+
+    
+    with open(archivo_salida, 'w') as f_salida:
+       
+        with open(archivo, 'r', encoding='latin-1') as f_entrada:
+            
+            for linea in f_entrada:
+               
+                palabra = linea.strip()
+                
+                if len(palabra) >= 8:
+                    f_salida.write(palabra + '\n')
 
     print(f"Se han guardado las palabras con 8 o más caracteres en el archivo {archivo_salida}.")
 
